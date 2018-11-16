6967fa80ab5c9537af8f0d8225c97f682eb3cf3a
Emerson Siega <emersonsiega@hotmail.com>
2018-11-16 19:34:44 -0200

    feat: Add redux logger

--- frontend/src/store/middleware/index.js
+++ frontend/src/store/middleware/index.js
@@ -1,8 +1,14 @@
-import thunk from 'redux-thunk'
 import { applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+const logger = createLogger({
+    duration: true
+})
 
 const middleware = applyMiddleware(
     thunk,
+    logger,
 )
 
 export default middleware
