a8272eba57dc3628931c86c66de0efa43c5cb23d
Emerson Siega <emersonsiega@hotmail.com>
2018-12-03 07:51:17 -0200

    feat: Post not found page

--- frontend/src/components/presentational/PostNotFound.js
+++ frontend/src/components/presentational/PostNotFound.js
@@ -1,7 +1,55 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FaLongArrowAltLeft, FaSadCry } from 'react-icons/fa'
+
+import { Link, IconZoom } from './Components'
+
+const PostNotFoundContainer = styled.section`
+    height: calc(100vh - 65px);
+    margin: auto;
+`
+
+const Content = styled.div`
+    top: 30vh;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const PostNotFoundMessage = styled.p`
+    font-size: 4em;
+    font-weight: 200;
+    margin: 0;
+`
+
+const HomeLink = styled(Link)`
+    font-weight: 300;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`
+
+const Icon = styled(IconZoom)`
+    color: ${props => props.theme.hover};
+    font-size: 4em;
+    display: flex;
+    align-items: center;
+` 
 
 const PostNotFound = () => (
-    <h1>Post not found =(</h1>
+    <PostNotFoundContainer>
+        <Content>
+            <Icon>
+                <FaSadCry />
+            </Icon>
+            <PostNotFoundMessage>Post not found</PostNotFoundMessage>
+            <HomeLink to='/'>
+                <FaLongArrowAltLeft /> Back to home
+            </HomeLink>
+        </Content>
+    </PostNotFoundContainer>
 )
 
 export default PostNotFound
