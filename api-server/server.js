fbd82c7050087d7774ad5e8847912e7578a61ab9
Emerson Siega <emersonsiega@hotmail.com>
2018-12-03 22:33:54 -0200

    chore: Build process

--- api-server/server.js
+++ api-server/server.js
@@ -112,7 +112,7 @@ app.get('/', (req, res) => {
   res.send(help)
 })
 
-app.use((req, res, next) => {
+app.use('/api/*', (req, res, next) => {
   const token = req.get('Authorization')
 
   if (token) {
@@ -125,8 +125,13 @@ app.use((req, res, next) => {
   }
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+    })
+}
 
-app.get('/categories', (req, res) => {
+app.get('/api/categories', (req, res) => {
     categories.getAll(req.token)
       .then(
           (data) => res.send(data),
@@ -139,7 +144,7 @@ app.get('/categories', (req, res) => {
       )
 })
 
-app.get('/:category/posts', (req, res) => {
+app.get('/api/:category/posts', (req, res) => {
     posts.getByCategory(req.token, req.params.category)
       .then(
           (data) => res.send(data),
@@ -152,7 +157,7 @@ app.get('/:category/posts', (req, res) => {
       )
 })
 
-app.get('/posts', (req, res) => {
+app.get('/api/posts', (req, res) => {
     posts.getAll(req.token)
       .then(
           (data) => res.send(data),
@@ -165,7 +170,7 @@ app.get('/posts', (req, res) => {
       )
 })
 
-app.post('/posts', bodyParser.json(), (req, res) => {
+app.post('/api/posts', bodyParser.json(), (req, res) => {
     posts.add(req.token, req.body)
       .then(
           (data) => res.send(data),
@@ -178,7 +183,7 @@ app.post('/posts', bodyParser.json(), (req, res) => {
       )
 })
 
-app.get('/posts/:id', (req, res) => {
+app.get('/api/posts/:id', (req, res) => {
     posts.get(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -191,7 +196,7 @@ app.get('/posts/:id', (req, res) => {
       )
 })
 
-app.delete('/posts/:id', (req, res) => {
+app.delete('/api/posts/:id', (req, res) => {
     posts.disable(req.token, req.params.id)
       .then(post => comments.disableByParent(req.token, post))
       .then(
@@ -205,7 +210,7 @@ app.delete('/posts/:id', (req, res) => {
       )
 })
 
-app.post('/posts/:id', bodyParser.json(), (req, res) => {
+app.post('/api/posts/:id', bodyParser.json(), (req, res) => {
     const { option } = req.body
     const id = req.params.id
     posts.vote(req.token, id, option)
@@ -220,7 +225,7 @@ app.post('/posts/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.put('/posts/:id', bodyParser.json(), (req, res) => {
+app.put('/api/posts/:id', bodyParser.json(), (req, res) => {
     posts.edit(req.token, req.params.id, req.body)
       .then(
         (data) => res.send(data),
@@ -233,7 +238,7 @@ app.put('/posts/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get('/api/posts/:id/comments', (req, res) => {
     comments.getByParent(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -246,7 +251,7 @@ app.get('/posts/:id/comments', (req, res) => {
       )
 })
 
-app.get('/comments/:id', (req, res) => {
+app.get('/api/comments/:id', (req, res) => {
     comments.get(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -259,7 +264,7 @@ app.get('/comments/:id', (req, res) => {
       )
 })
 
-app.put('/comments/:id', bodyParser.json(), (req, res) => {
+app.put('/api/comments/:id', bodyParser.json(), (req, res) => {
     comments.edit(req.token, req.params.id, req.body)
       .then(
         (data) => res.send(data),
@@ -272,7 +277,7 @@ app.put('/comments/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.post('/comments', bodyParser.json(), (req, res) => {
+app.post('/api/comments', bodyParser.json(), (req, res) => {
     comments.add(req.token, req.body)
       .then(
           (data) => res.send(data),
@@ -285,7 +290,7 @@ app.post('/comments', bodyParser.json(), (req, res) => {
       )
 })
 
-app.post('/comments/:id', bodyParser.json(), (req, res) => {
+app.post('/api/comments/:id', bodyParser.json(), (req, res) => {
     const { option } = req.body
     comments.vote(req.token, req.params.id, option)
       .then(
@@ -299,7 +304,7 @@ app.post('/comments/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.delete('/comments/:id', (req, res) => {
+app.delete('/api/comments/:id', (req, res) => {
     comments.disable(req.token, req.params.id)
       .then(
           (data) => res.send(data),
